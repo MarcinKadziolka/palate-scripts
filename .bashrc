de53e2936800ee5da02693b5a855aaefdc66bebb
MarcinKadziolka <kadziolka.marcin@outlook.com>
2025-12-16 14:49:49 +0100

    Add bash setup

--- .bashrc
+++ .bashrc
@@ -98,11 +98,11 @@ alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo
 
 # Alias definitions.
 # You may want to put all your additions into a separate file like
-# ~/.bash_aliases, instead of adding them here directly.
+# ~/.alias, instead of adding them here directly.
 # See /usr/share/doc/bash-doc/examples in the bash-doc package.
 
-if [ -f ~/.bash_aliases ]; then
-    . ~/.bash_aliases
+if [ -f ~/.alias ]; then
+    . ~/.alias
 fi
 
 # enable programmable completion features (you don't need to enable
@@ -134,4 +134,4 @@ fi
 unset __conda_setup
 # <<< conda initialize <<<
 
-export PATH="$HOME/scripts/new:$PATH"
+export PATH="$HOME/scripts/utils:$PATH"
--- package_management_scripts/install.sh
+++ package_management_scripts/install.sh
@@ -2,40 +2,39 @@
 #SBATCH --job-name=install_packages
 #SBATCH --partition=cpu
 #SBATCH --mem-per-cpu=5G
-cd $HOME/palate
+cd $HOME/scripts
 export PATH=$HOME/miniconda3/bin:$PATH
-
 if [[ "$1" == "" ]] ; then
     echo "Choose option:"
     echo "1. pip install -r requirements"
-    echo "2. conda env create -f environment.yml"
-    echo "3. conda env update --file environment.yml --prune"
+    echo "2. conda env create -f continued.yaml"
+    echo "3. conda env update --file continued.yaml --prune"
     echo "4. conda install <package>"
     echo "5. conda clean --packages"
     echo "6. conda env list"
     echo "7. conda list"
-    echo "9. conda remove -n palate --all -y"
+    echo "9. conda remove -n continued --all -y"
     exit 0
 fi
 
 if [[ "$1" == "1" ]] ; then
     echo "1. pip install -r requirements"
-    source activate palate
+    source activate continued
     pip install -r requirements.txt 
 elif [[ "$1" == "2" ]] ; then
-    echo "2. conda env create -f environment.yml"
+    echo "2. conda env create -f continued.yaml"
     source activate base
-    conda env create -f environment.yml
+    conda env create -f continued.yaml
 elif [[ "$1" == "3" ]] ; then
-    echo "3. conda env update --file environment.yml --prune"
-    conda env update --file environment.yml --prune
+    echo "3. conda env update --file continued.yaml --prune"
+    conda env update --file continued.yaml --prune
 elif [[ "$1" == "4" ]] ; then
     echo "4. conda install <package>"
-    source activate palate
+    source activate continued
     conda install $2
 elif [[ "$1" == "5" ]] ; then
     echo "5. conda clean --packages"
-    source activate palate
+    source activate continued
     conda clean --packages
 elif [[ "$1" == "6" ]] ; then
     echo "6. conda env list"
@@ -43,12 +42,12 @@ elif [[ "$1" == "6" ]] ; then
     conda env list
 elif [[ "$1" == "7" ]] ; then
     echo "7. conda list"
-    source activate palate
+    source activate continued
     conda list
 elif [[ "$1" == "9" ]] ; then
-    echo "9. conda remove -n palate --all -y"
+    echo "9. conda remove -n continued --all -y"
     source activate base
-    conda remove -n palate --all -y
+    conda remove -n continued --all -y
 else
     echo "Option doesn't exist: " $1
 fi
